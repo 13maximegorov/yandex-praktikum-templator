04b89d947d48db71912fea1e7af9aadbe67af128
Maxim Egorov <13maximegorov@gmail.com>
2021-02-17 15:27:08 +0500

    update

--- templator.js
+++ templator.js
@@ -1,13 +1,26 @@
 (function() {
+  let liArray = []
+
   class Templator {
     constructor(template) {
       this._template = template;
     }
 
     compile(ctx) {
-      return "<p>Моя разметка живет тут</p>";
+      if (this._template.match(/<li>/)) {
+        liArray.push(this._template.replace('{{name}}', ctx.name))
+      }
+      window.handleClick = ctx.handleClick
+      if (this._template.match(/ul/)) {
+        return this._template
+          .replace('{{ className }}', ctx.className)
+          .replace('{{chat.id}}', ctx.chat.id)
+          .replace('{{handleClick}}', 'window.handleClick()')
+          .replace('{{ items }}', liArray.join(''))
+      }
     }
   }
 
   window.Templator = Templator;
 })();
+
